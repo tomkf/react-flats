1223e0cb57f1b3139b34e77a16557d4d28eddb7c
Thomas Ferris <thomaskeithferris@gmail.com>
2019-08-03 16:59:18 -0400

    bug

--- src/components/FlatList.jsx
+++ src/components/FlatList.jsx
@@ -17,7 +17,7 @@ export default class FlatList extends React.Component {
 render() {
   const displayFlatItems = this.props.flats.map(flatItem => (  <Flat id={flatItem.id} key={flatItem.id} flat={flatItem} />))
          return (
-            <div className="flat-cards">
+            <div className="flat-list">
             {displayFlatItems}
             </div>
         )
