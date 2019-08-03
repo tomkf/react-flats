678751850f28936d8d8eb394eab9d91616ebde64
Thomas Ferris <thomaskeithferris@gmail.com>
2019-08-03 18:43:07 -0400

    update with click method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
 import React, {Component} from 'react';
 import FlatList from './FlatList.jsx';
-import Map from './Map.jsx';
 import flatData from '../../data/flats.js'
+import GoogleMapReact from 'google-map-react';
+
+
+//ad handlers for flat
+
+//pass this selected area to
+
 
 class App extends Component {
       constructor(props) {
         super(props);
         this.state = {
-          flatCollection: flatData
+          defaultFlat = flatData[0],
+          flatCollection: flatData,
+          center: {lat: defaultFlat.lat,
+            lng: defaultFlat.lng}
         };
+      }
+
+      updateDefault(selectedCard){
+       this.setState({defaultFlat: flatData[selectedCard]})
       }
 
 render(){
     return(<div className="content-box">
     <FlatList flats = {this.state.flatCollection} />
-     <Map /> 
+    <div className="map-container">
+          <GoogleMapReact defaultCenter={this.state.center} defaultZoom={12}>
+            <Marker lat={this.state.defaultFlat.lat} lng={this.state.defaultFlat.lng} />
+          </GoogleMapReact>
+        </div>
     </div>)
 }
 }
--- src/components/Flat.jsx
+++ src/components/Flat.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 
-// example flat: 
-// {
-//     name: "Charm at the Steps of the Sacre Coeur/Montmartre",
-//     imageUrl:
-//       "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-//     price: 164,
-//     priceCurrency: "EUR",
-//     lat: 48.884211,
-//     lng: 2.34689
-//   }
-
 export default class Flat extends React.Component {
+
+handleClick = event => {
+event.preventDefault();
+this.props.updateDefault(index)
+}
 
     render() {
              return (
                 <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${this.props.flat.imageUrl}')` }}>
                 <div className="card-category">
-                {this.props.flat.price}
+                {this.props.flat.price} {this.props.flat.priceCurrency}
                 </div>
                 <div className="card-description">
                 <h2>{this.props.flat.name}</h2>
                 </div>
-                <a className="card-link" href="#"></a>
+                <a className="card-link" href="#" onClick={this.handleClick}></a>
                 </div>
             )
         }
